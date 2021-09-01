2f56ffcf7f929d8e04221544c29d08fb3df1a234
unknown <Mauricio2801>
2021-09-01 16:52:07 -0600

    Añadiendo vista acerca del programa

---
+++ about.php
@@ -0,0 +1,70 @@
+<section class="about">
+        <div class="container" id="history">
+            <h1>Historia</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aperiam ratione, dicta, blanditiis consectetur mollitia ipsam dolores cumque ad laudantium repellendus, excepturi exercitationem nemo! Harum distinctio eos est veritatis maiores officia
+                dolorem autem deserunt obcaecati. Quo minus ducimus omnis iure, voluptates ex rem natus ut placeat tempora saepe doloribus hic consectetur aliquam earum neque, nihil et maxime dolores dignissimos. Rerum repudiandae adipisci, quibusdam
+                blanditiis laboriosam totam aliquam distinctio doloribus, consequatur molestias aspernatur et praesentium sunt animi minus illum magnam unde suscipit, ullam consequuntur tempora! Unde nobis atque sed aspernatur nihil qui eveniet, quisquam
+                est cupiditate. <br> Illum et soluta debitis aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam praesentium nemo necessitatibus perspiciatis unde? Odit, neque magni sint ex consectetur, quis aspernatur
+                adipisci ratione ullam provident ipsam. Quo similique at ratione nisi eaque earum iure temporibus incidunt error totam quibusdam asperiores neque modi tenetur, repellendus ex aut nesciunt sed impedit! Incidunt consequuntur laboriosam,
+                blanditiis ratione pariatur ducimus esse soluta in, est aspernatur maiores et cumque nulla? Eum, nisi. Provident illum fuga repellendus? Cum commodi aliquid, cumque tempore numquam repudiandae fuga aperiam dolorem similique quae animi.
+                Deserunt incidunt facilis obcaecati soluta hic, cupiditate quidem ducimus porro recusandae excepturi cum ad, officia quisquam fugiat commodi eius quia neque nostrum rerum eos rem voluptatum sapiente! Quod fugiat quam dolores ducimus nobis
+                ratione deserunt, doloremque excepturi repellat iure dolorum! Libero omnis reiciendis quaerat temporibus. Vero atque quis nam repudiandae, placeat provident expedita dolore. Id tenetur excepturi facere. Facere repudiandae architecto nihil
+                animi voluptate, tempora fugit magnam reprehenderit temporibus optio fugiat veritatis quis? Rerum nesciunt error vero, inventore mollitia modi natus. Ab incidunt sunt omnis alias sapiente natus dolores ad aperiam. Accusamus dolor praesentium
+                iste culpa eligendi error nihil iure amet dolores ipsam sed fugiat asperiores quibusdam doloribus sit, vero voluptates, similique libero temporibus! Temporibus.
+            </p>
+        </div>
+        <div class="container" id="p-dean">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, officiis.</h1>
+            <div class="text-wrapper">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aperiam ratione, dicta, blanditiis consectetur mollitia ipsam dolores cumque ad laudantium repellendus, excepturi exercitationem nemo! Harum distinctio eos est veritatis maiores officia
+                    dolorem autem deserunt obcaecati. Quo minus ducimus omnis iure, voluptates ex rem natus ut placeat tempora saepe doloribus hic consectetur aliquam earum neque, nihil et maxime dolores dignissimos. Rerum repudiandae adipisci, quibusdam
+                    blanditiis laboriosam totam aliquam distinctio doloribus, consequatur molestias aspernatur et praesentium sunt animi minus illum magnam unde suscipit, ullam consequuntur tempora! Unde nobis atque sed aspernatur nihil qui eveniet, quisquam
+                    est cupiditate. <br> Illum et soluta debitis aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam praesentium nemo necessitatibus perspiciatis unde? Odit, neque magni sint ex consectetur, quis aspernatur
+                    adipisci ratione ullam provident ipsam. Quo similique at ratione nisi eaque earum iure temporibus incidunt error totam quibusdam asperiores neque modi tenetur, repellendus ex aut nesciunt sed impedit! Incidunt consequuntur laboriosam,
+                    blanditiis ratione pariatur ducimus esse soluta in, est aspernatur maiores et cumque nulla? Eum, nisi. Provident illum fuga repellendus? Cum commodi aliquid, cumque tempore numquam repudiandae fuga aperiam dolorem similique quae animi.
+                    Deserunt incidunt facilis obcaecati soluta hic, cupiditate quidem ducimus porro recusandae excepturi cum ad, officia quisquam fugiat commodi eius quia neque nostrum rerum eos rem voluptatum sapiente! Quod fugiat quam dolores ducimus nobis
+                    ratione deserunt, doloremque excepturi repellat iure dolorum! Libero omnis reiciendis quaerat temporibus. Vero atque quis nam repudiandae, placeat provident expedita dolore. Id tenetur excepturi facere. Facere repudiandae architecto nihil
+                    animi voluptate, tempora fugit magnam reprehenderit temporibus optio fugiat veritatis quis? Rerum nesciunt error vero, inventore mollitia modi natus. Ab incidunt sunt omnis alias sapiente natus dolores ad aperiam. Accusamus dolor praesentium
+                    iste culpa eligendi error nihil iure amet dolores ipsam sed fugiat asperiores quibusdam doloribus sit, vero voluptates, similique libero temporibus! Temporibus.
+                </p>
+                <img src="" alt="Foto del Padre Dean">
+            </div>
+        </div>
+        <div class="container" id="memoria">
+            <h1>Lorem, ipsum dolor.</h1>
+            <article>
+                <header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, velit. Commodi ducimus, molestiae illum nam exercitationem id esse mollitia distinctio? Velit quisquam suscipit sit consectetur, exercitationem dicta, animi eligendi doloribus inventore assumenda molestias officia nisi! Libero, earum, similique error dicta veritatis quasi cumque iste adipisci quaerat officia, labore minima provident?</p>
+                </header>
+                <img src="" alt="Dean Brackley">
+            </article>
+            <article>
+                <header>
+                    <img src="" alt="Dean Brackley">
+                </header>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, velit. Commodi ducimus, molestiae illum nam exercitationem id esse mollitia distinctio? Velit quisquam suscipit sit consectetur, exercitationem dicta, animi eligendi doloribus inventore assumenda molestias officia nisi! Libero, earum, similique error dicta veritatis quasi cumque iste adipisci quaerat officia, labore minima provident?</p>
+            </article>
+            <article>
+                <header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, velit. Commodi ducimus, molestiae illum nam exercitationem id esse mollitia distinctio? Velit quisquam suscipit sit consectetur, exercitationem dicta, animi eligendi doloribus inventore assumenda molestias officia nisi! Libero, earum, similique error dicta veritatis quasi cumque iste adipisci quaerat officia, labore minima provident?</p>
+                </header>
+                <img src="" alt="Dean Brackley">
+            </article>
+        </div>
+        <div class="container" id="martires">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, reprehenderit.</h1>
+            <div>
+                <img src="" alt="Foto vigilia en honor de los martires">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat iusto magnam a, deleniti quam, praesentium voluptate veniam, quaerat porro excepturi blanditiis. Dolores quo, perspiciatis minima, dignissimos libero blanditiis distinctio quia fugiat facilis unde excepturi aliquam nostrum! Exercitationem dolores odit ducimus culpa obcaecati optio porro aut. Quis ex ducimus corrupti laboriosam! In minus qui nesciunt harum? Esse aliquid expedita animi numquam ipsum facilis commodi. Ullam, consequatur dolores fugiat cum libero assumenda repudiandae at sint. Dolores voluptas natus soluta possimus ipsa ad pariatur. Accusamus sequi voluptatem quod voluptates! Deserunt, voluptatibus repellendus! Odio saepe asperiores quod quam similique nostrum vitae sint dolorum aperiam alias eveniet dolore rerum tenetur reprehenderit, assumenda voluptate esse iure quasi inventore tempora? Sit corporis deleniti, modi porro temporibus rerum earum ducimus iste hic laborum eos quis veritatis enim dolore architecto sed maiores tempora, minima autem illum quibusdam ratione sunt quam eveniet. Aspernatur dolores sunt nam est numquam minima eveniet quidem deleniti fugit explicabo saepe, debitis reiciendis vitae voluptatem assumenda minus optio alias velit ipsa iusto voluptatum expedita cum placeat veniam. Magnam cum deserunt excepturi quis? Necessitatibus temporibus dignissimos ad? Dolores, optio. Cupiditate rem sunt optio tempora libero doloribus distinctio amet, id, provident ullam quaerat perferendis ratione reprehenderit ipsam!</p>
+            </div>
+        </div>
+        <div class="container" id="voluntarios">
+            <h1>Lorem.</h1>
+            <div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, vitae tempora? Harum, ratione dolore, delectus non ad consectetur aliquid tempore expedita officia veritatis nisi corrupti ipsum a recusandae tempora vitae alias in eaque, dicta perspiciatis unde cum excepturi. Consequatur, dolores!</p>
+                <img src="" alt="Voluntarios">
+            </div>
+        </div>
+    </section>
---
+++ start.php
@@ -0,0 +1,79 @@
+<section class="start">
+        <div class="container" id="founder">
+            <img src="" alt="Foto Padre Dean Brackley">
+            <div>
+                <h1>Fundador</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi quaerat aperiam placeat magni earum nesciunt maiores, aut quod maxime dolor ipsum quos perferendis, aliquid sint expedita natus. Vitae, iste suscipit!</p>
+            </div>
+        </div>
+        <div class="container" id="news">
+            <h1>Noticias</h1>
+            <div class="news-container">
+                <article>
+                    <header>
+                        <h3>Lorem ipsum dolor sit.</h3>
+                    </header>
+                    <p>
+                        <!--noticia-->
+                    </p>
+                </article>
+                <article>
+                    <header>
+                        <h3>Lorem ipsum dolor sit.2</h3>
+                    </header>
+                    <p>
+                        <!--noticia-->
+                    </p>
+                </article>
+                <article>
+                    <header>
+                        <h3>Lorem ipsum dolor sit.3</h3>
+                    </header>
+                    <p>
+                        <!--noticia-->
+                    </p>
+                </article>
+            </div>
+        </div>
+        <div class="container" id="phrase">
+            <img src="" alt="Foto padre Dean Brackley">
+            <div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, reprehenderit!</p>
+            </div>
+        </div>
+        <div class="container" id="community">
+            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+            <div class="community-container">
+                <article>
+                    <header>
+                        <img src="" alt="Foto de becario">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, qui.</p>
+                    </header>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis eveniet itaque neque illum necessitatibus labore optio omnis, ad ab aperiam doloribus nulla aliquam sequi? Aut quasi modi cum iusto officiis eveniet corrupti impedit repellendus similique
+                        illo harum voluptas fugit inventore aperiam soluta eum dolor, esse maiores ad! Fugit, eveniet quo.
+                    </p>
+                </article>
+                <article>
+                    <header>
+                        <img src="" alt="Foto de becario">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, qui.</p>
+                    </header>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis eveniet itaque neque illum necessitatibus labore optio omnis, ad ab aperiam doloribus nulla aliquam sequi? Aut quasi modi cum iusto officiis eveniet corrupti impedit repellendus similique
+                        illo harum voluptas fugit inventore aperiam soluta eum dolor, esse maiores ad! Fugit, eveniet quo.
+                    </p>
+                </article>
+                <article>
+                    <header>
+                        <img src="" alt="Foto de becario">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, qui.</p>
+                    </header>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis eveniet itaque neque illum necessitatibus labore optio omnis, ad ab aperiam doloribus nulla aliquam sequi? Aut quasi modi cum iusto officiis eveniet corrupti impedit repellendus similique
+                        illo harum voluptas fugit inventore aperiam soluta eum dolor, esse maiores ad! Fugit, eveniet quo.
+                    </p>
+                </article>
+            </div>
+        </div>
+    </section>
