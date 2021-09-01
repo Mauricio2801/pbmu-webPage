443c739f95a6ab717eb4bf25c8c49bf577d3185b
unknown <Mauricio2801>
2021-08-31 23:41:17 -0600

    importando a wordpress

--- header.php
+++ header.php
@@ -7,12 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <header class="main-header">
-        <img src="resources/logo_pbmu.png" alt="Logo">
+        <?php
+        echo "<img class=\"logo-image\" src=\"http://localhost/wordpress/wordpress/wp-content/uploads/2021/09/logo_pbmu.png\" alt=\"Logo\" />";
+        ?>
         <nav class="nav nav-pills nav-fill">
             <a class="nav-link active" aria-current="page" href="#">Inicio</a>
             <a class="nav-link" href="#">Sobre el programa</a>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php get_header();?>
+<?php get_body();?>
+<?php get_footer();?>
