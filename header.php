4e90126115a02cf529da14de1ea8f0bfb269deda
gabrielegr97 <enrique@silverlinecentralamerica.com>
2021-09-01 23:06:50 -0600

    some css

--- header.php
+++ header.php
@@ -12,28 +12,47 @@
 
 <body>
     <header class="main-header">
-        <?php
-        echo "<img class=\"logo-image\" src=\"http://localhost/wordpress/wordpress/wp-content/uploads/2021/09/logo_pbmu.png\" alt=\"Logo\" />";
-        ?>
-        <nav class="nav nav-pills nav-fill">
-            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-            <a class="nav-link" href="#">Sobre el programa</a>
-            <a class="nav-link" href="#">Becarios</a>
-            <a class="nav-link" href="#">Multimedia</a>
-            <a class="nav-link" href="#">Donaciones</a>
-            <a class="nav-link" href="#">Contactos</a>
-        </nav>
+    <img class="logo-img" src="http://localhost/wordpress/wp-content/uploads/2021/08/Copia-de-Logo-neutro1.png">    
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div id="navbar">
+    <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Sobre el Programa</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Becarios</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Multimedia</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Donaciones</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contactos</a>
+  </li>
+</ul>
+    </div>
+  
+</nav>
     </header>
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="" class="d-block w-100" alt="imagen1">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/09/DSC_1555-2000.jpg" class="d-block w-100" alt="imagen1">
             </div>
             <div class="carousel-item">
-                <img src="" class="d-block w-100" alt="imagen2">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/09/Copia-de-1-Portada-2000.jpg" class="d-block w-100" alt="imagen2">
             </div>
             <div class="carousel-item">
-                <img src="" class="d-block w-100" alt="imagen3">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/09/DSC00419-2000.jpg" class="d-block w-100" alt="imagen3">
+            </div>
+            <div class="carousel-item">
+                <img src="http://localhost/wordpress/wp-content/uploads/2021/09/Foto-de-Vinculacion-con-graduados-2000.jpg" class="d-block w-100" alt="imagen3">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
