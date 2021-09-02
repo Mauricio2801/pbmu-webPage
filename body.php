ac3c845b9f6733c5d7107a72455ac0d5772f6a05
gabrielegr97 <enrique@silverlinecentralamerica.com>
2021-09-01 21:56:53 -0600

    some color

---
+++ body.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/0a2e5ffc00.js" crossorigin="anonymous"></script>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <footer class="main-footer">
+        <p>
+            Copyrigth<i class="far fa-copyright"></i> 2020, Becas Mártires de la UCA <br> Universidad 
+            Centromericana "José Simeón Cañas" <br> Centro Monseñor Romero. Boulevar Los Próceres, San Salvador, Centroamérica <br> Télefono: (503)22106675
+        </p>
+        <div>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-youtube"></i>
+            <i class="fab fa-instagram"></i>
+        </div>
+    </footer>
+</body>
+
+</html>
