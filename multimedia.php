348f777a8233bcaae598dc00c1a22fca5e7bf89d
gabrielegr <00199518uca.edu.sv>
2021-09-04 14:51:46 -0500

    galleries

--- multimedia.php
+++ multimedia.php
@@ -1,39 +1,43 @@
 <section class="multimedia">
-    <div class="container" id="news">
-        <h1>Noticias</h1>
-        <div class="news-wrapper">    
-            <div class="news-container">
-                <div class="news-title">
-                    <h5>Lorem ipsum dolor sit.</h5>
-                        
-                </div>
-                <div class="news-title">
-                    <h5>Lorem ipsum dolor sit.2</h5>
-                </div>
-                <div class="news-title">
-                    <h5>Lorem ipsum dolor sit.3</h5>
-                </div>
-            </div>
-            <img class="news-img" src="http://localhost/wordpress/wp-content/uploads/2021/09/81-yKbVND-L.png" >
-        </div>
+    <div id="photo-gallery">
+        <p>CONOCE LAS DIFERENTES ACTIVIDADES Y MEMORIAS DEL PROGRAMA EN NUESTRA</p>
+        <h1>GALERÍA FOTOGRÁFICA</h1>
+        <div class="gallery-wrapper">
+        <img class="one" src="">
+        <img class="two" src="">
+        <img class="three" src="">
+        <img class="four" src="">
+        <img class="five" src="">
+        <img class="six" src="">
     </div>
-    <div class="container" id="boletin">
-        <h1 class="titulo tituloB">Boletin Informativo</h1>
-        <div class="boletin-wrapper">    
-            <img class="boletin-img" src="http://localhost/wordpress/wp-content/uploads/2021/09/81-yKbVND-L.png" >
-            <div class="boletin-container">
-                <div class="boletin-title">
-                    <h5>Lorem ipsum dolor sit.</h5>
-                        
-                </div>
-                <div class="boletin-title">
-                    <h5>Lorem ipsum dolor sit.2</h5>
-                </div>
-                <div class="boletin-title">
-                    <h5>Lorem ipsum dolor sit.3</h5>
-                </div>
-            </div>
-        </div>
     </div>
-    <!--Falta agregar espacio para las fotos y videos-->
+    <div id="video-gallery">
+        <p>LA MEMORIA DEL LEGADO DE P. DEAN EN</p>
+        <h1>VIDEO</h1>
+        <div class="gallery-wrapper">
+        <video class ="one" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+       
+        </video> 
+        <video class ="two" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+        
+        </video> 
+        <video class ="three" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+
+        </video> 
+        <video class ="four" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+        </video> 
+        <video class ="five" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+        
+        </video> 
+        <video class ="six" width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+       
+        </video> 
+    </div>
+    </div>
 </section>
